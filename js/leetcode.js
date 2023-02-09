b6438b9959ffd6a895c13c4e0a245e35aabf4307
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-09 11:05:53 -0600

    TwoSum

--- js/leetcode.js
+++ js/leetcode.js
@@ -1,5 +1,26 @@
 
+// 1. TwoSum
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let vals = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if(target - nums[i] in vals) {
+            return [vals[target - nums[i]], i];
+        } else {
+            vals[nums[i]] = i
+        }
+    }
+    return [];
+};
+
+
+//
 
 
 

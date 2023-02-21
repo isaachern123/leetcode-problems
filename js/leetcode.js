a099376644f4f3c3ac831418b1bf7256cc438ff7
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-21 10:23:46 -0600

    Running Sum of 1d Array

--- js/leetcode.js
+++ js/leetcode.js
@@ -139,3 +139,18 @@ var mySqrt = function(x) {
     let newNumber = Math.sqrt(x);
     return Math.floor(newNumber);
 };
+
+
+
+// 1480. Running Sum of 1d Array
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums
+};
